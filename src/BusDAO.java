a87b30ed8f5f52e99e8d0110650dd166b1a3df28
sekar N <sekarnagarajan1234@gmail.com>
2023-09-17 16:29:45 +0530

    java

--- src/BusDAO.java
+++ src/BusDAO.java
@@ -1,23 +1,28 @@
 import com.mysql.cj.protocol.x.ReusableOutputStream;
 
 import java.sql.Connection;
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-//Data Access Object
 public class BusDAO {
-  public void  displayBusInfo() throws SQLException {
-      String query = "select * from bus";
-      Connection con =  DbConnection.getConnection();
-      Statement st = con.createStatement();
-      ResultSet rs = st.executeQuery(query);
+    public void displayBusInfo() throws SQLException {
+        String query = "Select * from bus";
+        Connection con = DbConnection.getConnection();
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(query);
 
-      while(rs.next()) {
-          System.out.println("Bus No: " + rs.getInt(1));
-          if(rs.getInt(2)==0)
-              System.out.println("AC: no ");
-          else
-              System.out.println("AC: yes ");
-          System.out.println("Capacity: " + rs.getInt(3));
-      }
+        while (rs.next()) {
+            System.out.println("Bus No: " + rs.getInt(1));
+            if (rs.getInt(2) == 0)
+                System.out.println("AC: no ");
+            else
+                System.out.println("AC: yes ");
+            System.out.println("Capacity: " + rs.getInt(3));
+        }
+
+        System.out.println("------------------------------------------");
     }
+
+    
 }
