4ccf81b5d27c21a2367d13e5f3190324d67ba96c
sekar N <sekarnagarajan1234@gmail.com>
2023-09-17 16:18:24 +0530

    java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,6 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
  class DbConnection {
     private static final String url = "jdbc:mysql://localhost:3306/busresv";
