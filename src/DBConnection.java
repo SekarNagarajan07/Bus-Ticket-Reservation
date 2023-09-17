1e35ea50787adb7c563030cccdff810998c3e9d6
sekar N <sekarnagarajan1234@gmail.com>
2023-09-17 16:01:49 +0530

    java

---
+++ src/DBConnection.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+ class DbConnection {
+    private static final String url = "jdbc:mysql://localhost:3306/busresv";
+    private static final String userName = "sekar";
+    private static final String passWord = "Sekar2002@";
+
+    public static Connection getConnection() throws SQLException {
+        return  DriverManager.getConnection(url,userName,passWord);
+    }
+
+}
