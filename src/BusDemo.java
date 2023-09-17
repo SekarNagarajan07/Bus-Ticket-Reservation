ec14b5ca1a204102d2aecbba2a71d5acbf1f7c5b
sekar N <sekarnagarajan1234@gmail.com>
2023-09-17 16:29:57 +0530

    java

--- src/BusDemo.java
+++ src/BusDemo.java
@@ -1,10 +1,11 @@
 import busResv.Bus;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BusDemo {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws SQLException {
 
 /*
    Without jdbc connection
@@ -17,27 +18,27 @@ public class BusDemo {
 
 BusDAO busdao = new BusDAO();
 busdao.displayBusInfo();
-    int userOptions = 1;
-    Scanner sc = new Scanner(System.in);
-
-    for(Bus b : buses) {
-      b.displayBusInfo();
-    }
-
-    while (userOptions == 1) {
-      System.out.println("Enter 1 to Book and 2 to Exit : ");
-      userOptions = sc.nextInt();
-      if (userOptions == 1) {
-        Booking booking = new Booking();
-        if(booking.isAvailable(bookings, buses)) {
-          bookings.add(booking);
-          System.out.println("Your booking is confirmed");
-        }
-        else {
-          System.out.println("Sorry, seat is full. Try another bus or date.");
-        }
-      }
-    }
+//    int userOptions = 1;
+//    Scanner sc = new Scanner(System.in);
+//
+//    for(Bus b : buses) {
+//      b.displayBusInfo();
+//    }
+//
+//    while (userOptions == 1) {
+//      System.out.println("Enter 1 to Book and 2 to Exit : ");
+//      userOptions = sc.nextInt();
+//      if (userOptions == 1) {
+//        Booking booking = new Booking();
+//        if(booking.isAvailable(bookings, buses)) {
+//          bookings.add(booking);
+//          System.out.println("Your booking is confirmed");
+//        }
+//        else {
+//          System.out.println("Sorry, seat is full. Try another bus or date.");
+//        }
+//      }
+//    }
 
   }
 }
