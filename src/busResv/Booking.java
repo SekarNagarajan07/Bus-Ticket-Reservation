b145301e0bfe185308ba00b89ad6c9954a88f272
sekar N <sekarnagarajan1234@gmail.com>
2023-09-18 14:18:08 +0530

    java

--- src/busResv/Booking.java
+++ src/busResv/Booking.java
@@ -1,42 +1,41 @@
-import busResv.Booking1DAO;
-
+package busResv;
+import java.util.*;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
-
 public class Booking {
   String passengerName;
   int busNo;
   Date date;
 
-  Booking() {
-    Scanner sc = new Scanner(System.in);
+  Booking(){
+    Scanner scanner = new Scanner(System.in);
     System.out.println("Enter name of passenger: ");
-    passengerName = sc.next();
+    passengerName = scanner.next();
     System.out.println("Enter bus no: ");
-    busNo = sc.nextInt();
+    busNo = scanner.nextInt();
     System.out.println("Enter date dd-mm-yyyy");
-    String dateInput = sc.next();
+    String dateInput = scanner.next();
     SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 
     try {
       date = dateFormat.parse(dateInput);
     } catch (ParseException e) {
-      throw new RuntimeException(e);
+      // TODO Auto-generated catch block
+      e.printStackTrace();
     }
+
   }
 
-  public boolean isAvailable() throws SQLException {
+  public boolean isAvailable()  throws SQLException{
+
     BusDAO busdao = new BusDAO();
     Booking1DAO bookingdao = new Booking1DAO();
     int capacity = busdao.getCapacity(busNo);
 
-   int booked = bookingdao.getBookedCount(busNo,date);
-    return booked < capacity;
+    int booked = bookingdao.getBookedCount(busNo,date);
+
+    return booked<capacity;
+
   }
 }
-
-
-
